3b40705168c2b13ac044f53be9f4eac12d367643
Mona Lisa <125455854+swervolee@users.noreply.github.com>
2024-07-03 21:18:49 +0300

    Data obfuscation

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,12 +6,29 @@ import re
 from typing import List
 
 
-def filter_datum(fields: List,
+def filter_datum(fields: List[str],
                  redaction: str,
                  message: str,
                  separator: str) -> str:
     """
     Obfuscates data
+
+    Args:
+        fields (List[str]):
+        A list of strings representing all fields to obfuscate.
+
+        redaction (str):
+        A string representing by what the field will be obfuscated.
+
+        message (str):
+        A string representing the log line.
+
+        separator (str):
+        A string representing by which character is
+        separating all fields in the log line (message).
+
+    Returns:
+        str: The log message with specified fields obfuscated.
     """
     pattern = f"({'|'.join(fields)})=[^{separator}]*"
     return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}", message)
